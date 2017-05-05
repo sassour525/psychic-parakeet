98c49556ad45150237254ed5573ca9512a19c025
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-05-04 20:52:23 -0500

    commented js / route files

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,7 @@ $document.ready(function() {
     var emailInput = $('input#email');
     var passwordInput = $('input#password');
 
+    //grab values from the loginForm once submitted
     loginForm.on('submit', function(event) {
         event.preventDefault();
         var userData = {
@@ -10,16 +11,20 @@ $document.ready(function() {
             password: passwordInput.val().trim()
         }
 
+        //if any values are missing do not proceed
         if (!userData.email || !userData.password) {
             return;
         }
 
+        //call loginUser so it can post the data to authenticate
         loginUser(userData.email, userData.password);
 
+        //clear input fields
         emailInput.val('');
         passwordInput.val('');
     });
 
+    //post data to authenticate the user
     function loginUser(email, password) {
         $.post('api/login', {
             email: email,
--- public/js/register.js
+++ public/js/register.js
@@ -5,6 +5,7 @@ $document.ready(function() {
     var passwordInput = $('input#password');
     var companyInput = $('input#company');
 
+    //grab values from the signUpForm once submitted
     signUpForm.on('submit', function(event) {
         var userData = {
             name: nameInput.val().trim(),
@@ -13,18 +14,22 @@ $document.ready(function() {
             company: companyInput.val().trim()
         };
 
+        //if any values are missing do not proceed
         if (!userData.name || !userData.email || !userData.password || !userData.company) {
             return;
         }
 
+        //call createUser so it can post the new user data
         createUser(userData.name, userData.email, userData.password, userData.company);
 
+        //clear input fields
         nameInput.val('');
         emailInput.val('');
         passwordInput.val('');
         companyInput.val('');
     });
 
+    //function to post new user data to route to create a user in the DB
     function createUser(name, email, password, company) {
         $.post('/api/signup', {
             name: name,
