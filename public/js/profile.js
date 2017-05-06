f7b796e5bed3546eb3641e1e2cdade9dde20a9df
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-05-06 13:32:44 -0500

    added class to profile card and added dynamic user details

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+    $.get('/api/user_data').then(function(user) {
+        var userData = user;
+        console.log(userData);
+        $('#user-card').append('<p>Employee ID: ' + userData.id + '</p>');
+        $('#user-card').append('<p>Email: ' + userData.email + '</p>');
+    });
+
     //log the user out
     $('#logout').on('click', function() {
         $.post('/logout').then(function(data) {
@@ -8,5 +15,4 @@ $(document).ready(function() {
             console.log(err);
         });
     });
-    
 });
