226ddc6e8797827e4a7be80119405529d170ed29
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-05-06 12:45:29 -0500

    adding logout functionality

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+
+    //log the user out
     $('#logout').on('click', function() {
-        $.post('/logout');
+        $.post('/logout').then(function(data) {
+            window.location.replace(data);
+        }).catch(function(err) {
+            console.log(err);
+        });
     });
+    
 });
